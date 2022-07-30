0d00bb61652da1e2953f33089f2bf5fc3175b2d1
bagasrizqila <bagasrizqila123@gmail.com>
2022-07-30 12:26:04 +0700

    main py

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
-#3Sebuah script utama main.py yang terdiri dari sebuah logic sederhana yang menjawab sebuah use case sederhana
+#3Sebuah script utama main.py yang terdiri dari sebuah logic sederhana yang menjawab sebuah use case sederhana 
 
-from gpiozero import LightSensor
-ldr = LightSensor(4)
-while True:
-	print(ldr.value)
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+delayt = .1 
+value = 0 # this variable will be used to store the ldr value
+ldr = 7 #ldr is connected with pin number 7
+led = 11 #led is connected with pin number 11
+GPIO.setup(led, GPIO.OUT) # as led is an output device so that’s why we set it to output.
+GPIO.output(led, False) # keep led off by default 
+def rc_time (ldr):
+    count = 0
+ 
+    #Output on the pin for
+    GPIO.setup(ldr, GPIO.OUT)
+    GPIO.output(ldr, False)
+    time.sleep(delayt)
+ 
+    #Change the pin back to input
+    GPIO.setup(ldr, GPIO.IN)
+ 
+    #Count until the pin goes high
+    while (GPIO.input(ldr) == 0):
+        count += 1
+ 
+    return count
+ 
+ 
+#Catch when script is interrupted, cleanup correctly
+try:
+    # Main loop
+    while True:
+        print("Ldr Value:")
+        value = rc_time(ldr)
+        print(value)
+        if ( value <= 10000 ):
+                print("Lights are ON")
+                GPIO.output(led, True)
+        if (value > 10000):
+                print("Lights are OFF")
+                GPIO.output(led, False)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
